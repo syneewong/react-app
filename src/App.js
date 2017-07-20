e70700da92e38e58d18d189eb5f816d9872f953f
Syn-Ee Wong <synee.wong@ie.com.au>
2017-07-20 11:20:01 +1000

    Video 5 set state of components that is being triggered by input field.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
 
 class App extends React.Component {
+  constructor(){
+    super();
+
+    this.state = {
+      txt: 'this is the state txt',
+      cat: 0
+    }
+  }
+  update( e ){
+    this.setState({txt: e.target.value})
+    // this.setState({cat: e.target.value})
+  }
   render(){
     let txt = this.props.txt
     return (
         <div>
-          <h1>{txt}</h1>
-          <b>Bold</b>
+          <input type="text" onChange={this.update.bind(this)}/>
+          <h1>{this.state.txt} - {this.state.cat}</h1>
         </div>
       )
   }
