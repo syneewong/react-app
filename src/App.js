f5b79c895965c749900ced4c1053c176ac007861
Syn-Ee Wong <synee.wong@ie.com.au>
2017-07-20 14:30:23 +1000

    Video 14 create person component to be used as the iteration. Create filter method to match input fields value to filter data set.

--- src/App.js
+++ src/App.js
@@ -11,16 +11,29 @@ class App extends React.Component {
     .then ( response => response.json() )
     .then ( ({results: items}) => this.setState({items}))
   }
+  filter(e){
+    this.setState({
+      filter: e.target.value
+    })
+  }
   render(){
     let items = this.state.items
-    return(
+    if(this.state.filter){
+      items = items.filter ( item =>
+        item.name.toLowerCase()
+        .includes(this.state.filter.toLowerCase()))
+    }
+    return (
       <div>
+        <input type="text" onChange={this.filter.bind(this)}/>
         {items.map(item =>
-          <h4 key={item.name}>{item.name}</h4>)}
+          <Person key={item.name} person={item} />)}
       </div>
     )
   }
 }
+
+const Person = (props) => <h4>{props.person.name}</h4>
 
 App.defaultProps = {val: 0}
 
