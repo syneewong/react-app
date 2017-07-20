f93f2a06a8b77dfa70fbd52b7c2987603dba68eb
Syn-Ee Wong <synee.wong@ie.com.au>
2017-07-20 11:50:22 +1000

    Video 7 adding custom propType validation to react components.

--- src/App.js
+++ src/App.js
@@ -2,14 +2,21 @@ import React from 'react';
 
 class App extends React.Component {
   render(){
-    return <Title />
+    return <Title text="The hello" />
   }
 }
 
 const Title = (props) => <h1>Title: {props.text}</h1>
 
 Title.propTypes = {
-  text: React.PropTypes.string
+  text(props, propName, component){
+    if(!(propName in props)){
+      return new Error(`missing ${propName}`)
+    }
+    if(props[propName].length < 6){
+      return new Error(`${propName} was too short`)
+    }
+  }
 }
 
 export default App
