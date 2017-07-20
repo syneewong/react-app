775f5ddf69cbbeb808e4ee4db153b8c21cf70f3e
Syn-Ee Wong <synee.wong@ie.com.au>
2017-07-20 10:52:45 +1000

    Video 3 display output in react component’s render method.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
-// class App extends React.Component {
-//   render(){
-//     return <h1 className="">Hello World</h1>
-//   }
-// }
-
-const App = () => <h1>Hello stateless</h1>
+class App extends React.Component {
+  render(){
+    return (
+        <div>
+          <h1 className="">Hello World</h1>
+          <b>Bold</b>
+        </div>
+      )
+  }
+}
 
 export default App
