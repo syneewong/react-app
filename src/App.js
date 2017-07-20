cc58158314ccf6eaf155519565f9a558954b1c29
Syn-Ee Wong <synee.wong@ie.com.au>
2017-07-20 14:09:45 +1000

    Video 13 componentWillReceiveProps sets new state to true after init false.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {increasing: false}
+  }
   update(){
     ReactDOM.render(
       <App val={this.props.val+1} />,
       document.getElementById('root'))
   }
+  componentWillReceiveProps(nextProps){
+    this.setState({increasing: nextProps.val > this.props.val})
+  }
   render(){
+    console.log(this.state.increasing)
     return (
       <button onClick={this.update.bind(this)}>
         {this.props.val}
