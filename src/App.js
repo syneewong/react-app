52a5ef0a2248b86ba01166ce739f81bc57829f14
Syn-Ee Wong <synee.wong@ie.com.au>
2017-07-20 12:10:08 +1000

    Video 10 Two input without refs.

--- src/App.js
+++ src/App.js
@@ -3,29 +3,26 @@ import React from 'react';
 class App extends React.Component {
   constructor(){
     super();
-    this.state = {currentEvent: '---'}
-    this.update = this.update.bind(this)
+    this.state = {a: ''}
   }
   update( e ){
-    this.setState({currentEvent: e.type})
+    this.setState({
+      a: e.target.value,
+      b: e.target.value
+    })
   }
   render(){
     return (
       <div>
-          <textarea
-            onKeyPress={this.update}
-            onCopy={this.update}
-            onCut={this.update}
-            onPaste={this.update}
-            onFocus={this.update}
-            onBlur={this.update}
-            onDoubleClick={this.update}
-            onTouchStart={this.update}
-            onTouchMove={this.update}
-            onTouchEnd={this.update}
-            cols="30"
-            rows="10" />
-          <h1>{this.state.currentEvent}</h1>
+          <input
+            type="text"
+            onChange={this.update.bind(this)}
+          /> {this.state.a}
+          <hr />
+          <input
+            type="text"
+            onChange={this.update.bind(this)}
+          /> {this.state.b}
       </div>
     )
   }
