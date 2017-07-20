4571dfe980176b6d62039170be5b80c4c3a3e6e8
Syn-Ee Wong <synee.wong@ie.com.au>
2017-07-20 13:05:24 +1000

    Video 11 Mount and unmount in a `wrapper` component that calls an `app` component.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 class App extends React.Component {
   constructor(){
@@ -19,6 +20,27 @@ class App extends React.Component {
   componentDidMount(){
     console.log('componentDidMount')
   }
+  componentWillUnmount(){
+    console.log('componentWillUnmount')
+  }
 }
 
-export default App
+class Wrapper extends React.Component {
+  mount(){
+    ReactDOM.render(<App />, document.getElementById('a'))
+  }
+  unmount(){
+    ReactDOM.unmountComponentAtNode(document.getElementById('a'))
+  }
+  render(){
+    return(
+      <div>
+        <button onClick={this.mount.bind(this)}>Mount</button>
+        <button onClick={this.unmount.bind(this)}>Unmount</button>
+        <div id="a"></div>
+      </div>
+    )
+  }
+}
+
+export default Wrapper
