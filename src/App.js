3e028093e2a95c34b24227fa0bc8fa70dbcdc0e7
Syn-Ee Wong <synee.wong@ie.com.au>
2017-07-20 10:46:05 +1000

    Video 2 write “Hello World” with JSX.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React from 'react';
 
-const App = () => <h1>Hello</h1>
+// class App extends React.Component {
+//   render(){
+//     return <h1 className="">Hello World</h1>
+//   }
+// }
+
+const App = () => <h1>Hello stateless</h1>
 
 export default App
