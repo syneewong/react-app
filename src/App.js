d87e054a7c8213a8b5cddbcc89597caead950ffb
Syn-Ee Wong <synee.wong@ie.com.au>
2017-07-20 15:05:51 +1000

    Video 16 setting up stage for babel and CSS import.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import 'App.css';
 
 class App extends React.Component {
   constructor(){
     super();
-    this.state={
+    this.state = {
       input: '/* add your jsx here */',
       output: '',
       err: ''
