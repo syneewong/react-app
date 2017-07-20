04b0563f2cf708ab9b4196049da2b9b0996614a4
Syn-Ee Wong <synee.wong@ie.com.au>
2017-07-20 11:00:43 +1000

    Video 4 set properties on react components.

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import React from 'react';
 
 class App extends React.Component {
   render(){
+    let txt = this.props.txt
     return (
         <div>
-          <h1 className="">Hello World</h1>
+          <h1>{txt}</h1>
           <b>Bold</b>
         </div>
       )
   }
+}
+
+App.propTypes = {
+  txt: React.PropTypes.string,
+  cat: React.PropTypes.number.isRequired
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(
-	<App />,
+	<App cat={5} txt="this is a prop value" />,
 	document.getElementById('root')
 );
