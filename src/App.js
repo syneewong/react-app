b664c6534d312ccdf44c6adc5b3d37f852f3d0a9
Syn-Ee Wong <synee.wong@ie.com.au>
2017-07-20 11:53:58 +1000

    Video 9 Setting the stage.

--- src/App.js
+++ src/App.js
@@ -2,20 +2,9 @@ import React from 'react';
 
 class App extends React.Component {
   render(){
-    return <Title text="The hello" />
-  }
-}
-
-const Title = (props) => <h1>Title: {props.text}</h1>
-
-Title.propTypes = {
-  text(props, propName, component){
-    if(!(propName in props)){
-      return new Error(`missing ${propName}`)
-    }
-    if(props[propName].length < 6){
-      return new Error(`${propName} was too short`)
-    }
+    return (
+      <div></div>
+    )
   }
 }
 
